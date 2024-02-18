b2510f4aec8f966787e7a4800c5da1840eb23bee
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2024-02-18 23:02:55 +0200

    finalUpdate

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -3,7 +3,14 @@ import React from 'react';
 const HomePage = () => {
   return (
     <>
-      {/* <h1>Home</h1> */}
+      <div className="homeText">
+        <h1>
+          A company that provides car ornamentation services in Ukraine.
+        </h1>
+        <p className='homeItem'>Go to the catalog and choose a car!</p>
+      </div>
+
+      <img className="img" src="/CarRental-App/HomePage2.png" alt="CarFon" />
     </>
   );
 };
